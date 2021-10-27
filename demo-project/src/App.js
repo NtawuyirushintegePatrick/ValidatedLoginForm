af5ac6c612184f3833dae8a00b360585627cd7db
Ntawuyirushintege Patrick <ntawuyirushintegepatrick@gmail.com>
2021-10-27 11:05:18 +0200

    first commit

---
+++ demo-project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './style.css';
+import ValidatedLoginForm from './ValidatedLoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+      <h1>Validated Login Form</h1>
+      <ValidatedLoginForm/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
